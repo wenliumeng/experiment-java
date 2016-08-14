fef5c2215d1030d5bfdc6f5aedba7875b6a51b27
seymour <120465271@qq.com>
2016-08-14 18:28:00 +0800

    init

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import org.apache.commons.collections.OrderedMap;
+import org.apache.commons.collections.map.LinkedMap;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/30.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        char c = 'g';
+        System.out.println(Integer.toBinaryString((int)c));
+    }
+}
