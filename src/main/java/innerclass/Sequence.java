5ce5753ae19d051014bc63ae15fa3a1fe12f889b
seyMour <120465271@qq.com>
2016-10-11 10:25:09 +0800

    内部类

--- src/main/java/innerclass/Sequence.java
+++ src/main/java/innerclass/Sequence.java
@@ -3,9 +3,11 @@ package innerclass;
 public class Sequence {
     private Object[] items;
     private int next = 0;
+    private String text2 = "";
 
-    public Sequence(int size) {
+    public Sequence(int size, String text2) {
         items = new Object[size];
+        this.text2 = text2;
     }
 
     public void add(Object x) {
@@ -31,20 +33,54 @@ public class Sequence {
             if (i < items.length)
                 i++;
         }
+
+        public void print(){
+            System.out.println(text2);
+        }
+    }
+
+    class Inter {
+        public void print(){
+            System.out.println(text2);
+        }
+
+        public Sequence getSequence(){
+            return Sequence.this;
+        }
     }
 
     public Selector selector() {
         return new SequenceSelector();
     }
 
+    public Inter inter(){
+        return new Inter();
+    }
+
     public static void main(String[] args) {
-        Sequence sequence = new Sequence(5);
+        Sequence sequence = new Sequence(5,"dd");
         for (int i = 0; i < 5; i++)
             sequence.add(i + "");
         Selector selector = sequence.selector();
-        while (!selector.end()){
+        while (!selector.end()) {
             System.out.println(selector.current());
             selector.next();
         }
+
+        Sequence sequence1 = new Sequence(5,"ddd");
+        for (int i = 0; i < 5; i++)
+            sequence1.add(new Text1());
+        Selector selector1 = sequence1.selector();
+        while (!selector1.end()) {
+            ((Text1) selector1.current()).print();
+            selector1.next();
+        }
+
+        Inter inter = sequence1.inter();
+        inter.print();
+        //通过内部类对象取得外部类对象的引用
+        Sequence sequence2 = inter.getSequence();
+        //通过外部类对象生成内部类对象，没有外部类对象之前是不可能有内部类对象的，嵌套类（静态内部类）除外
+        Sequence.Inter inter1 = sequence2.new Inter();
     }
 }
---
+++ src/main/java/innerclass/Text1.java
@@ -0,0 +1,8 @@
+package innerclass;
+
+public class Text1 {
+    String str1 = "";
+    void print(){
+        System.out.println(str1);
+    }
+}
