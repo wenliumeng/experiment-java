e1cb8c4d93ae701982dce4389d35bc49ab4be261
seymour <120465271@qq.com>
2016-11-18 23:57:21 +0800

    classinfo RTTI

--- src/main/java/classinfo/Shapes.java
+++ src/main/java/classinfo/Shapes.java
@@ -1,8 +1,46 @@
 package classinfo;
 
-/**
- * Created by seymour on 2016/11/18.
- */
-public class Shapes {
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * RTTI
+ * Created by seyMour on 2016/11/18.
+ * ☞seyMour☜
+ */
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    @Override
+    public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    @Override
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    @Override
+    public String toString() {
+        return "Triangle";
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        List<Shape> shapes = Arrays.asList(new Circle(), new Square(), new Triangle());
+        shapes.forEach(Shape::draw);
+    }
 }
