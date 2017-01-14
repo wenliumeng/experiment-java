08549de9680914ee87e5490b3b19d6ecb4284cf2
seyMour <120465271@qq.com>
2017-01-14 10:31:46 +0800

    从标准流中读取

--- src/main/java/javaio/Echo.java
+++ src/main/java/javaio/Echo.java
@@ -1,8 +1,7 @@
 package javaio;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  * 从标准流中读取
@@ -10,9 +9,14 @@ import java.io.InputStreamReader;
  */
 public class Echo {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        //标准输入System.in不能直接用，必须包装后才能
+        /*BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String s;
         while ((s = in.readLine()) != null && s.length() != 0)
-            System.out.println(s);
+            System.out.println(s);*/
+
+        //标准输出流System.out可以直接使用
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("hello world");
     }
 }
