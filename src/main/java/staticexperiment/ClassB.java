1a35c1b6b23fcb78cd42849bb007f0fc6d5cd55b
seymour <120465271@qq.com>
2017-04-09 20:13:15 +0800

    静态 类 方法

--- src/main/java/staticexperiment/ClassB.java
+++ src/main/java/staticexperiment/ClassB.java
@@ -1,13 +1,19 @@
 package staticexperiment;
 
-
+/**
+ * 先执行AB的静态代码，再生成A类和对象，
+ */
 public class ClassB extends ClassA {
-    public ClassB() {
+
+    public ClassB()
+    {
         System.out.println("Object B");
     }
+
     {
         System.out.println("Class B");
     }
+
     static {
         System.out.println("static B");
     }
