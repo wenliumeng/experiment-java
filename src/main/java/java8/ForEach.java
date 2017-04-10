dc2dfc878a0c11bfdf3d645b85e2be1c3523e3cb
seymour-mac <me@baofei-tech.com>
2017-04-10 22:55:03 +0800

    java8新特性

---
+++ src/main/java/java8/ForEach.java
@@ -0,0 +1,71 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 采用java8中的新流式特性来替代foreach循环
+ * Created by seymour on 17/4/10.
+ */
+public class ForEach {
+    private class Article {
+
+        private final String title;
+        private final String author;
+        private final List<String> tags;
+
+        private Article(String title, String author, List<String> tags) {
+            this.title = title;
+            this.author = author;
+            this.tags = tags;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public List<String> getTags() {
+            return tags;
+        }
+    }
+
+    public Article getFirstJavaArticle1(List<Article> articles) {
+
+        for (Article article : articles) {
+            if (article.getTags().contains("Java")) {
+                return article;
+            }
+        }
+
+        return null;
+    }
+
+    public Optional<Article> getFirstJavaArticle2(List<Article> articles) {
+        return articles.stream()
+                .filter(article -> article.getTags().contains("Java"))
+                .findFirst();
+    }
+
+    public void example1(){
+        List<Article> articles = new ArrayList<>();
+        Article article1 = new Article("title1","zhangsan", Arrays.asList("tag1","tag2"));
+        Article article2 = new Article("title2","lisi", Arrays.asList("tag3","tag4"));
+        Article article3 = new Article("title3","wangwu", Arrays.asList("tag5","tag6"));
+        articles.add(article1);
+        articles.add(article2);
+        articles.add(article3);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
